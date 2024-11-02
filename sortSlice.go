acef92712c619499f1bb182a6e53b99d485b1740
blessedsibanda263 <blessedsibanda263@gmail.com>
2024-11-02 18:32:55 +0200

    Sorting slices

---
+++ sortSlice.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	sInts := []int{1, 0, 2, -3, 4, -20}
+	fmt.Println("sInts original:", sInts)
+	sort.Ints(sInts)
+	fmt.Println("sInts:", sInts)
+	sort.Sort(sort.Reverse(sort.IntSlice(sInts)))
+	fmt.Println("Reverse:", sInts)
+
+	fmt.Printf("\n\n")
+
+	sFloats := []float64{1.0, 0.2, 0.22, -3, 4.1, -0.1}
+	fmt.Println("sFloats original:", sFloats)
+	sort.Float64s(sFloats)
+	fmt.Println("sFloats:", sFloats)
+	sort.Sort(sort.Reverse(sort.Float64Slice(sFloats)))
+	fmt.Println("Reverse:", sFloats)
+
+	fmt.Printf("\n\n")
+
+	sStrings := []string{"aa", "a", "A", "Aa", "aab", "AAa"}
+	fmt.Println("sStrings original:", sStrings)
+	sort.Strings(sStrings)
+	fmt.Println("sStrings:", sStrings)
+	sort.Sort(sort.Reverse(sort.StringSlice(sStrings)))
+	fmt.Println("Reverse:", sStrings)
+}
